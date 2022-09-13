2c25cb4a498e633409f8966b0ce31c587a271495
elangsmdr <elangs1911@gmail.com>
2022-09-13 14:15:08 +0700

    Initial commit

---
+++ app/src/main/java/com/example/view_binding_10636/TaskList.kt
@@ -0,0 +1,11 @@
+package com.example.view_binding_10636
+
+object TaskList {
+    val TaskList = listOf<Task>(
+        Task("Chapter 1", "Bangun Tidur", "Kebiasaan sehari hari makin Indah"),
+        Task("Chapter 2", "Nguli Genshin Impact", "Mencari Primo buat Gacha Mama Yaemiko tercintas / Kegung Wangi"),
+        Task("Chapter 3", "Ngopi", "Habis capek Nguli, kan enak ngopi ya kan"),
+        Task("Chapter 4", "dimana mama Ei ?", "Buat Adu Mekanik Showcase Ayaka Pyro"),
+        Task("Chapter 5", "Upload ke Tiktok",  "Buat Pamer ke view Showcase Ayaka Pyro C6 + Bennet C6")
+    )
+}
